91fa47b99ce8416c257b41d94d77a7cdf68b303c
Nordseele <47527859+nordseele@users.noreply.github.com>
2023-07-22 11:16:38 +0200

    PIO

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 /*
     Hans 2 converts OSC UDP and serial MIDI to II and sends that over I2C to ER-301 and TXo. 
     Version 2 runs on RPI PICO W and is programmed in C. This is the first draft. 
-    Hardware and enclosure exist, not yet available, not open sourced. 
     Personal project, work in progress.
     Version: 0.2
     Recent changes: Now using PIO for serial MIDI IN.
